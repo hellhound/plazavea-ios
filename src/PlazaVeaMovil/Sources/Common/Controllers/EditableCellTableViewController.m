e9ef3b01517e377bdcf68bc3d7bd75a4e4f9ada3
Jean-Pierre Chauvel <jchauvel@gmail.com>
2011-08-05 10:54:42 -0500

    Removiendo cabeceras innecesarias [#PVMI-5]

--- src/PlazaVeaMovil/Sources/Common/Controllers/EditableCellTableViewController.m
+++ src/PlazaVeaMovil/Sources/Common/Controllers/EditableCellTableViewController.m
@@ -1,9 +1,7 @@
-#import <stdlib.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
-#import "Common/Additions/NSError+Additions.h"
 #import "Common/Controllers/EditableCellTableViewController.h"
 
 @implementation EditableCellTableViewController
