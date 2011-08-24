f1915b405d541725f35b66a74e9028582054702b
Jean-Pierre Chauvel <jchauvel@gmail.com>
2011-08-24 12:50:38 -0500

    oops~ Three20 requiere que implemente el inicializador initWithNibName:bundle:, aún si se trata de un controlador de UITableView (en UIKit el inicializador por defecto es initWithStyle:) [#PVMI-12]

--- src/PlazaVeaMovil/Sources/Recipes/RecipesController.m
+++ src/PlazaVeaMovil/Sources/Recipes/RecipesController.m
@@ -2,10 +2,23 @@
 
 #import <Three20/Three20.h>
 
+#import "Recipes/Constants.h"
 #import "Recipes/AlphabeticalRecipesDataSource.h"
 #import "Recipes/RecipesController.h"
 
 @implementation RecipesController
+
+#pragma mark -
+#pragma mark TTViewController
+
+- (id)initWithNibName:(NSString *)nibName bundle:(NSBundle *)bundle
+{
+    if ((self = [super initWithNibName:nibName bundle:bundle]) != nil) {
+        [self setTableViewStyle:UITableViewStylePlain];
+        [self setTitle:kRecipesTitle];
+    }
+    return self;
+}
 
 #pragma mark -
 #pragma mark TTTableViewController
