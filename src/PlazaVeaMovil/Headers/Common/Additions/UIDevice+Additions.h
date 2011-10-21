92d813cc38c8d5841a76e0eebb060374344ae3e0
Jean-Pierre Chauvel <jchauvel@gmail.com>
2011-10-21 15:00:20 -0500

    Añadiendo una categoría a UIDevice para determinar la versión del sistema operativo de manera sencilla [#PVMI-42]

---
+++ src/PlazaVeaMovil/Headers/Common/Additions/UIDevice+Additions.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    kSystemVersion3,
+    kSystemVersion32,
+    kSystemVersion4,
+    kSystemVersion5,
+    kSystemVersion6andUp
+} UIDeviceSystemVersion;
+
+@interface UIDevice (Additions)
+
+- (UIDeviceSystemVersion)deviceSystemVersion;
+@end
