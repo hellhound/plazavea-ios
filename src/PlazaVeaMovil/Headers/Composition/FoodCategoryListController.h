c491e5fc596814a58d8bd9a36ca04ea396b8158b
Luis Carlos Larrañaga <caliopigio@gmail.com>
2011-11-17 18:02:23 -0500

    Implementando lista de categorías de comidas del módulo Composición nutricionañ [#PVMI-66]

---
+++ src/PlazaVeaMovil/Headers/Composition/FoodCategoryListController.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "Common/Controllers/EditableCellTableViewController.h"
+#import "Composition/Models.h"
+
+@interface FoodCategoryController : EditableCellTableViewController
+{
+    UINavigationItem *_navItem;
+}
+
+@end
