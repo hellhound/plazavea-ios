1bee85a8d27e97a6640e8faf4919eb1210cfab0f
Luis Carlos Larrañaga <caliopigio@gmail.com>
2012-01-23 10:15:53 -0500

    Añadiendo PageCurlButton a Commons [#PVMI-121+

---
+++ src/PlazaVeaMovil/Headers/Common/Views/PageCurlButton.h
@@ -0,0 +1,39 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol PageCurlButtonDelegate;
+
+@interface PageCurlButton: UIButton
+{
+@private
+	UIView *targetView_;
+	BOOL isTargetViewCurled_;
+	BOOL isAnimating_;
+	BOOL hidesWhenAnimating_; // Default to NO
+	BOOL hidesTargetViewWhileCurled_; // Default to YES    
+	id <PageCurlControlDelegate> delegate_;
+	NSTimer *animationTimer_;
+	NSTimeInterval curlAnimationDuration_;
+	NSTimeInterval curlAnimationShouldStopAfter_;
+	NSTimer *timer_;
+}
+@property (nonatomic, retain) UIView *targetView;
+@property (nonatomic, assign, readonly) BOOL isTargetViewCurled;
+@property (nonatomic, assign) BOOL hidesWhenAnimating;
+@property (nonatomic, assign) BOOL hidesTargetViewWhileCurled;
+@property (nonatomic, assign) id <PageCurlControlDelegate> delegate;
+@property (nonatomic, assign) NSTimeInterval curlAnimationDuration;
+@property (nonatomic, assign) NSTimeInterval curlAnimationShouldStopAfter;
+- (void)curlViewUp;
+- (void)curlViewDown;
+- (void)cancelCurlingAnimation;
+@end
+
+@protocol PageCurlButtonDelegate <NSObject>
+
+@optional
+- (void)pageCurlButtonWillCurlViewUp:(PageCurlControl *)control;
+- (void)pageCurlButtonDidCurlViewUp:(PageCurlControl *)control;
+- (void)pageCurlButtonWillCurlViewDown:(PageCurlControl *)control;
+- (void)pageCurlButtonDidCurlViewDown:(PageCurlControl *)control;
+@end
